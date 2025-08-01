ed868f58f47c7e12a71ed9f60a3a2f2cf36c39ac
Anand <anand.work2005@gmail.com>
2025-08-01 18:40:19 +0530

    adding the svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  return(
+    <div className='container'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="" alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>About</li>
+      </ul>
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <img src="" alt="" className="theme-toggle" />
+    </div>
+  )
+}
+
+export default Navbar
