18fd04c7937f66ce5bc2d686b8285c37dfa79b6b
Anand <anand.work2005@gmail.com>
2025-08-02 16:48:08 +0530

    add my name in the navabr

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,4 @@
 import './Navbar.css'
-import logo_light from '../../assets/logo-black.png'
-import logo_dark from '../../assets/logo-white.png'
 import search_icon_light from '../../assets/search-w.png'
 import search_icon_dark from '../../assets/search-b.png'
 import toggle_light from '../../assets/night.png'
@@ -9,13 +7,14 @@ import toggle_dark from '../../assets/day.png'
 
 function Navbar({ theme, setTheme }) {
 
+
   const toggleMode = () => {
     theme === 'dark' ? setTheme('light') : setTheme('dark');
   }
 
   return (
     <div className="navbar">
-      <img src={theme === 'dark' ? logo_dark : logo_light} alt="" className="logo" />
+      <h1>Anand Kumar</h1>
       <ul>
         <li>Home</li>
         <li>Projects</li>
