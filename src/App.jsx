88a7a714468cf13c019d5a09b0b609b7393a0062
Anand <anand.work2005@gmail.com>
2025-08-02 16:27:24 +0530

    add the navbar with toggle theme section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const currTheme = localStorage.getItem('currTheme');
+
+  const [theme, setTheme] = useState(currTheme ? currTheme : 'dark');
+  useEffect(() => {
+    localStorage.setItem('currTheme', theme);
+  }, [theme]);
+
   return(
-    <div className='container'>
-      <Navbar />
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
-function Navbar() {
+import './Navbar.css'
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from '../../assets/logo-white.png'
+import search_icon_light from '../../assets/search-w.png'
+import search_icon_dark from '../../assets/search-b.png'
+import toggle_light from '../../assets/night.png'
+import toggle_dark from '../../assets/day.png'
+
+
+function Navbar({ theme, setTheme }) {
+
+  const toggleMode = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+  }
+
   return (
     <div className="navbar">
-      <img src="" alt="" className="logo" />
+      <img src={theme === 'dark' ? logo_dark : logo_light} alt="" className="logo" />
       <ul>
         <li>Home</li>
         <li>Projects</li>
@@ -9,9 +23,10 @@ function Navbar() {
       </ul>
       <div className="search-box">
         <input type="text" placeholder="Search" />
+        <img src={theme === 'dark' ? search_icon_dark : search_icon_light} alt='' />
       </div>
 
-      <img src="" alt="" className="theme-toggle" />
+      <img onClick={() => toggleMode()} src={theme === 'dark' ? toggle_dark : toggle_light} alt="" className="theme-toggle" />
     </div>
   )
 }
