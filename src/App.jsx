afaa781445047dd95a2b78e42f408f72c73a7dca
Anand <anand.work2005@gmail.com>
2025-08-17 21:25:48 +0530

    starting again

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 import { useEffect, useState } from 'react'
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
 
 function App() {
-  const currTheme = localStorage.getItem('currTheme');
-
-  const [theme, setTheme] = useState(currTheme ? currTheme : 'dark');
-  useEffect(() => {
-    localStorage.setItem('currTheme', theme);
-  }, [theme]);
-
-  return(
-    <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <Footer />
-    </div>
-  );
 }
 
 export default App
