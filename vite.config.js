12c129e386e9de795b3ec97047fe56f03648c90c
Anand <anand.work2005@gmail.com>
2025-08-19 18:59:50 +0530

    add the base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  "base": "/portfolio",
   test: {
     globals: true,
     environment: 'jsdom',
